e2281ce427c0de59a37b10b3605ebb297cb2986c
Arthur Lima <artglima.dev@gmail.com>
2025-01-02 12:01:56 -0300

    feat: created Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { HeaderContainer, Logo } from "./styles"
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <Logo>
+        <h1>DSList</h1>
+      </Logo>
+    </HeaderContainer>
+  )
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`  
+  display: flex;
+  align-items: center;
+
+  background: var(--color-blue-dark);
+`;
+
+export const Logo = styled.div`
+  font-size: 12px;
+  font-weight: 400;
+  color: var(--color-white);
+
+  line-height: 150%;
+  padding: 2.5rem 1.5rem;
+`;
